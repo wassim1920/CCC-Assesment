b4f4a97238d5d24dc363763c6fdeec0c96975617
azizsayela <93082939+azizsayela@users.noreply.github.com>
2024-02-08 15:42:34 +0100

    -

--- server/controllers/userSubscription.js
+++ server/controllers/userSubscription.js
@@ -1,5 +1,15 @@
 // #######
 
-const MockData = [{}, {}, {}];
+const MockData = [
+  {
+    userEmail: "Ste159@gmail.com",
+  },
+  {
+    userEmail: "Ste159412@gmail.com",
+  },
+  {
+    userEmail: "Ste159412@cayonCloud.com",
+  },
+];
 
 exports.userSubscription = async (req, res) => {};
